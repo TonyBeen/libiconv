7ec84539eb9cef2dcd147d556487104a913304ee
eular <1772747354@qq.com>
2026-01-04 14:21:02 +0800

    update

--- cmake/dist.cmake
+++ cmake/dist.cmake
@@ -339,13 +339,7 @@ macro(iconv_configure_file)
   if(MSVC)
     string(TOUPPER "lib${arg_LIBRARY_NAME}" arg_library_name_upper)
     set(HAVE_VISIBILITY
-    "WIN32
-      #if defined(${arg_LIBRARY_NAME}_EXPORTS)
-        #define ${arg_library_name_upper}_DLL_EXPORTED __declspec(dllexport)
-      #else
-        #define ${arg_library_name_upper}_DLL_EXPORTED __declspec(dllimport)
-      #endif
-  #elif true"
+      "defined(__linux__)"
     )
   endif()
   configure_file(${arg_INPUT} ${arg_OUTPUT} ${arg_UNPARSED_ARGUMENTS})
